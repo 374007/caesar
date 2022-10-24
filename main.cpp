b920d1211a18d51fc0c2f0476acd502f92004370
Pavel Gerasin <374007@gmail.com>
2022-10-24 20:25:33 +0700

    fdf

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+std::string encrypt_caesar(std::string word, int inc)
+{
+ return 0;
+}
+
+std::string decrypt_caesar(std::string word, int inc)
+{
+ return 0;
+}
+
+
+int main() {
+    std::cout << "Caesar!" << std::endl;
+    return 0;
+}
