245ece7e81ede559b0909b44501f71bdf88f6f7c
Pavel Gerasin <374007@gmail.com>
2022-10-24 20:47:39 +0700

    fdf

--- main.cpp
+++ main.cpp
@@ -5,13 +5,23 @@ std::string encrypt_caesar(std::string word, int inc)
  return 0;
 }
 
-std::string decrypt_caesar(std::string word, int inc)
+std::string decrypt_caesar (std::string word, int inc)
 {
  return 0;
 }
 
+std::string phrase,crypt;
+int inc;
 
 int main() {
     std::cout << "Caesar!" << std::endl;
+    std::cout << "Input phrase:";
+    std::cin >> phrase;
+    std::cout << "Input shift:";
+    std::cin >> inc;
+    std::cout << "Crypt or Decrypt (y/n)?";
+    std::cin >> crypt;
+
+
     return 0;
 }
