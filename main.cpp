866a97bb602a565251ab41b997b655ce4edd5588
Pavel Gerasin <374007@gmail.com>
2022-10-24 22:51:10 +0700

    the end

--- main.cpp
+++ main.cpp
@@ -2,24 +2,26 @@
 
 std::string encrypt_caesar(std::string word, int inc)
 {
- for  (int i=0; i<word.length();i++) {
-   if ( word[i] >='a' && word[i]<='z') {
-     int count = int(word[i]) - int('a');
-     count +=inc;
-     word[i] = count%26;
-   }
-   else if ( word[i] >='A' && word[i]<='Z') {
-
-   }
-   else word[i]='*';
-
- }
- return word;
+  char alph;
+    for  (int i = 0; i < word.length(); i++) {
+       if ( word[i] >= 'a' && word[i] <= 'z') alph = 'a' ;
+       else if ( word[i] >= 'A' && word[i] <='Z') alph = 'A';
+       else continue;
+       word[i] = (word[i] - alph + inc) % 26 + alph;
+    }
+  return word;
 }
 
 std::string decrypt_caesar (std::string word, int inc)
 {
- return 0;
+   char alph;
+     for  (int i = 0; i < word.length(); i++) {
+        if ( word[i] >= 'a' && word[i] <= 'z') alph = 'z' ;
+        else if ( word[i] >= 'A' && word[i] <= 'Z') alph = 'Z';
+        else continue;
+        word[i] = (word[i] - alph - inc) % 26 + alph;
+     }
+   return word;
 }
 
 std::string phrase,crypt;
@@ -28,12 +30,12 @@ int inc;
 int main() {
     std::cout << "Caesar!" << std::endl;
     std::cout << "Input phrase:";
-    std::cin >> phrase;
+    std::getline(std::cin, phrase);
     std::cout << "Input shift:";
     std::cin >> inc;
-    std::cout << "Crypt or Decrypt (y/n)?";
-    std::cin >> crypt;
-    std::cout << encrypt_caesar(phrase,inc);
-
+   // std::cout << "Crypt or Decrypt (y/n)?";
+   // std::cin >> crypt;
+    std::cout <<"Crypt: " << encrypt_caesar(phrase,inc) << "\n";
+    std::cout <<"Decrypt: " << decrypt_caesar(encrypt_caesar(phrase, inc), inc);
     return 0;
 }
